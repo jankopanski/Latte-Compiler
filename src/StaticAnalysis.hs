ea4f93c2f860ddbaaf83a060cbda8d2a47e59bfc
Jan Kopański <j.kopanski@students.mimuw.edu.pl>
2017-12-12 22:56:07 +0100

    basic exprsessions

--- src/StaticAnalysis.hs
+++ src/StaticAnalysis.hs
@@ -1,12 +1,12 @@
 module StaticAnalysis where
 
-import qualified Data.Map as Map
+import qualified Data.Map             as Map
 
-import Control.Monad.State
-import Control.Monad.Except
-import Control.Monad.Reader
+import           Control.Monad.Except
+import           Control.Monad.Reader
+import           Control.Monad.State
 
-import AbsLatte
+import           AbsLatte
 
 type Name = String
 type TypeEnv = Map.Map Name (Type Position)
@@ -14,11 +14,14 @@ type TypeScope = (TypeEnv, TypeEnv)
 type Position = Maybe (Int, Int)
 data Error
   = Redefinition Name Position Position
+  | TypeMismatch (Type Position)
   | NoReturn Name Position
-  | InvalidReturn Position
+  | InvalidReturnType Position
 type Checker a = ExceptT Error (State TypeScope) a
-type StatementChecker a = ReaderT (Type ()) (ExceptT Error (State TypeScope)) a
-type ExpressionChecker = StatementChecker (Type ())
+type StatementChecker a = ReaderT (Type Position) (ExceptT Error (State TypeScope)) a
+type ExpressionChecker = StatementChecker (Type Position)
+-- type StatementChecker a = ReaderT (Type ()) (ExceptT Error (State TypeScope)) a
+-- type ExpressionChecker = StatementChecker (Type ())
 -- type StatementChecker = Checker (Maybe (Type Position))
 
 -- instance Eq Type where
@@ -63,10 +66,10 @@ addFnDecl (FnDef fnpos rettype (Ident name) args _) = do
       putOuther $ Map.insert name (Fun fnpos rettype argtypes) decls
 
 getPositionFromType :: Type Position -> Position
-getPositionFromType (Int pos) = pos
-getPositionFromType (Str pos) = pos
-getPositionFromType (Bool pos) = pos
-getPositionFromType (Void pos) = pos
+getPositionFromType (Int pos)     = pos
+getPositionFromType (Str pos)     = pos
+getPositionFromType (Bool pos)    = pos
+getPositionFromType (Void pos)    = pos
 getPositionFromType (Fun pos _ _) = pos
 
 getInner :: Checker TypeEnv
@@ -84,20 +87,24 @@ putOuther outenv = getInner >>= \inenv -> put (inenv, outenv)
 checkTopDef :: TopDef Position -> Checker ()
 checkTopDef (FnDef fpos ftype (Ident name) args block) = do
   mapM_ addArg args
-  ret <- runReaderT (checkBlock block) (void ftype)
+  ret <- runReaderT (checkBlock block) ftype--(void ftype)
   unless ret $ throwError (NoReturn name fpos)
 
 addArg :: Arg Position -> Checker ()
 addArg (Arg pos argtype (Ident name)) = do
   env <- getInner
   case Map.lookup name env of
-    Just t -> throwError (Redefinition name pos (getPositionFromType t))
+    Just t  -> throwError (Redefinition name pos (getPositionFromType t))
     Nothing -> putInner $ Map.insert name argtype env
 
+-- Bool oznacza czy wystąpił return czy nie
 checkBlock :: Block Position -> StatementChecker Bool
 checkBlock (Block _ stmts) = or <$> mapM checkStmt stmts
 
+-- Statements --
+
 checkStmt :: Stmt Position -> StatementChecker Bool
+
 checkStmt (Empty _) = return False
 
 checkStmt (BStmt _ block) = do
@@ -110,12 +117,57 @@ checkStmt (BStmt _ block) = do
 
 checkStmt (VRet pos) = do
   rtype <- ask
-  if rtype == Void () then return True else throwError (InvalidReturn pos)
+  if rtype == Void pos then return True else throwError (InvalidReturnType pos)
+--if rtype == Void () then return True else throwError (InvalidReturnType pos)
 
 checkStmt (Ret pos expr) = do
   rtype <- ask
   etype <- checkExpr expr
-  if rtype == etype then return True else throwError (InvalidReturn pos)
+  if rtype == etype then return True else throwError (InvalidReturnType pos)
+
+-- Expressions --
 
 checkExpr :: Expr Position -> ExpressionChecker
-checkExpr (ELitTrue pos) = return (Void ())
+
+checkExpr (ELitTrue pos) = return $ Bool pos
+
+checkExpr (ELitFalse pos) = return $ Bool pos
+
+checkExpr (ELitInt pos _) = return $ Int pos
+
+checkExpr (EString pos _) = return $ Str pos
+
+checkExpr (Neg pos expr) =
+  let negtype = Int pos in
+  checkExpr expr >>= \exprtype -> if exprtype == negtype
+    then return negtype
+    else throwError $ TypeMismatch negtype
+
+checkExpr (Not pos expr) =
+  let nottype = Bool pos in
+  checkExpr expr >>= \exprtype -> if exprtype == nottype
+    then return nottype
+    else throwError $ TypeMismatch nottype
+
+checkExpr (EAdd pos expr1 _ expr2) = checkBinOp expr1 expr2 (Int pos)
+
+checkExpr (EMul pos expr1 _ expr2) = checkBinOp expr1 expr2 (Int pos)
+
+checkExpr (EAnd pos expr1 expr2) = checkBinOp expr1 expr2 (Bool pos)
+
+checkExpr (EOr pos expr1 expr2) = checkBinOp expr1 expr2 (Bool pos)
+
+checkUnOp :: Expr Position -> Type Position -> ExpressionChecker
+checkUnOp expr optype= do
+  exprtype <- checkExpr expr
+  if exprtype == optype
+    then return optype
+    else throwError $ TypeMismatch optype
+
+checkBinOp :: Expr Position -> Expr Position -> Type Position -> ExpressionChecker
+checkBinOp expr1 expr2 optype= do
+  exprtype1 <- checkExpr expr1
+  exprtype2 <- checkExpr expr2
+  if exprtype1 == optype && exprtype2 == optype
+    then return optype
+    else throwError $ TypeMismatch optype
